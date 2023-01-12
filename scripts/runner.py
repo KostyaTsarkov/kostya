e6cc3a9e6abf86a48952a9872a7c3f22c3f24565
kostya <kent_7@list.ru>
2023-01-12 09:40:40 +0500

    Master

--- scripts/runner.py
+++ scripts/runner.py
@@ -1,2 +1,16 @@
-#pip install nornir
-from netmiko import ConnectHandler
+from nornir import InitNornir
+nr = InitNornir(
+    runner={
+        "plugin": "threaded",
+        "options": {
+            "num_workers": 100,
+        },
+    },
+    inventory={
+        "plugin": "SimpleInventory",
+        "options": {
+            "host_file": "inventory/hosts.yaml",
+            "group_file": "inventory/groups.yaml"
+        },
+    },
+)
